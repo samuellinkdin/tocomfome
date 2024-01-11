6eb60f8a6b5a6b94402f519e2a762926c8fea0ab
samuellinkdin <155685673+samuellinkdin@users.noreply.github.com>
2024-01-11 19:43:12 -0300

    adicionando recursos avançados
    (aprendi js a 20 minutos)

---
+++ script.js
@@ -0,0 +1,3 @@
+let usuario = prompt("Qual é o  seu nome?")
+let mensagem = " muito obrigado por nos visitar, escolha um dos nossos lanches delicioos e nos chame no whatsap clicando no botão abrir menu e logo em seguida em contato";
+window.alert(usuario + mensagem)
